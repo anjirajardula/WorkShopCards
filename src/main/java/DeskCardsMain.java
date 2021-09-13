566c7428463724850ea80afbad876abd9a580225
anjirajardula <anjirajardula@gmail.com>
2021-09-13 10:12:23 +0530

    Added:welcome to deck cards and playrs

---
+++ src/main/java/DeskCardsMain.java
@@ -0,0 +1,11 @@
+public class DeskCardsMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the deck of cards program");
+
+        PlayCards cardsPlay = new PlayCards();
+        cardsPlay.uniqueCards();
+
+
+    }
+}
